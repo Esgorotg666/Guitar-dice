bf2e180c9f5a9ccdea10d0782878475ff7b4f74c
Esgorotg666 <149533370+Esgorotg666@users.noreply.github.com>
2026-09-02 14:56:51 -0700

    Expose raw AvantLink auth string so their crawler can match it

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,7 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
+const AVANT_SRC = 'http://classic.avantlink.com/affiliate_app_confirm.php?mode=js&authResponse=cc541d53f4a80b0e684e927f407f35c5db9519d0';
+
 export default function Document() {
   return (
     <Html lang="en">
@@ -11,10 +13,21 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Oxanium:wght@300..800&family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
-        <script type="text/javascript" src="http://classic.avantlink.com/affiliate_app_confirm.php?mode=js&authResponse=cc541d53f4a80b0e684e927f407f35c5db9519d0" />
+        <script type="text/javascript" src={AVANT_SRC} />
       </Head>
       <body>
-        <script type="text/javascript" src="http://classic.avantlink.com/affiliate_app_confirm.php?mode=js&authResponse=cc541d53f4a80b0e684e927f407f35c5db9519d0" />
+        <script type="text/javascript" src={AVANT_SRC} />
+        <div
+          id="avantlink-confirm"
+          hidden
+          dangerouslySetInnerHTML={{
+            __html:
+              '<!-- ' + AVANT_SRC + ' -->' +
+              '<script type="text/javascript" src="' + AVANT_SRC + '"></script>' +
+              AVANT_SRC +
+              ' authResponse=cc541d53f4a80b0e684e927f407f35c5db9519d0'
+          }}
+        />
         <Main />
         <NextScript />
       </body>

30744264c3acf9da9f448685cc6011ac8ab8288b
Esgorotg666 <149533370+Esgorotg666@users.noreply.github.com>
2026-09-02 23:45:20 -0700

    Load genre theme stylesheet

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 import '../styles/classroom.css';
 import '../styles/theme.css';
+import '../styles/genre.css';
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }

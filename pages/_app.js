9653340e3b95dfd805c258f76e84ebbae540cf70
Esgorotg666 <149533370+Esgorotg666@users.noreply.github.com>
2026-09-02 23:29:19 -0700

    Load light theme stylesheet

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/globals.css';
 import '../styles/classroom.css';
+import '../styles/theme.css';
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }

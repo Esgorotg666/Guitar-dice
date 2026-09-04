11733af0f0a049e6fb7fb39e850a31a5beda7d12
Esgorotg666 <149533370+Esgorotg666@users.noreply.github.com>
2026-09-03 20:32:21 -0700

    Import locker styles

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import '../styles/globals.css';
 import '../styles/classroom.css';
 import '../styles/theme.css';
 import '../styles/genre.css';
+import '../styles/locker.css';
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }

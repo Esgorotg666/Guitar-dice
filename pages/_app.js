e0618d0f91001cd9b84a0d7abe818bedbf665ace
Esgorotg666 <149533370+Esgorotg666@users.noreply.github.com>
2026-09-07 17:59:22 -0700

    Count visits on every page

--- pages/_app.js
+++ pages/_app.js
@@ -5,9 +5,11 @@ import '../styles/genre.css';
 import '../styles/locker.css';
 import '../styles/diceTray.css';
 import WelcomeGate from '../components/WelcomeGate';
+import TrafficPing from '../components/TrafficPing';
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
+      <TrafficPing />
       <WelcomeGate />
       <Component {...pageProps} />
     </>
